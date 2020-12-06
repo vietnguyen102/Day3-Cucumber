aacaf8e476827aaa0cd936086b04d0fcf6c1cd21
vietnm <vietnm7597@gmail.com>
2020-12-06 23:56:59 +0700

    Add After hooks to close browser after run scenario

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,3 +2,13 @@ require 'selenium-webdriver'
 require 'rspec'
 
 $driver = Selenium::WebDriver.for :firefox
+
+$i = 0
+After do |scenario|
+    $i = $i+1
+    if $i==3
+        $driver.quit
+    end
+end
+
+
